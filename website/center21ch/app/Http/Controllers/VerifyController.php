38dba82db30037401ec28c3e5602582b22050f65
Ali <aalkinan@myune.edu.au>
2018-06-12 09:46:55 +1000

    Create the viryfied token function will take the token amd check in the database ? success , fail

--- website/center21ch/app/Http/Controllers/VerifyController.php
+++ website/center21ch/app/Http/Controllers/VerifyController.php
@@ -4,7 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
 class VerifyController extends Controller
 {
-    //
+
+
+
+    public function verify($token)
+    {
+
+        User::where('token',$token)->firstOrFail()
+
+        ->update(['token'=>null]);
+        
+        return redirect()->home()->with('succes','acount verifyied');
+        
+        
+    }
 }
