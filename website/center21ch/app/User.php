3a8a6073e7822497b0ad781500b4db24721a4d5d
Ali <aalkinan@myune.edu.au>
2018-06-12 09:59:55 +1000

    implement App\Notifications\verifyEmail class

--- website/center21ch/app/User.php
+++ website/center21ch/app/User.php
@@ -5,6 +5,8 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use App\Notifications\verifyEmail;
+
 class User extends Authenticatable
 {
     use Notifiable;
