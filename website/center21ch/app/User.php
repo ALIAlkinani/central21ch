e41eedd91df5b0451258f204b9b57a0001835c9b
Ali <aalkinan@myune.edu.au>
2018-06-12 09:54:48 +1000

    create verifyed method and sendVerfifycationEmail

--- website/center21ch/app/User.php
+++ website/center21ch/app/User.php
@@ -26,4 +26,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function verifyed()
+    {
+        return $this->token ===null;
+    }
+
+    public function sendVerifycationEmail()
+    {
+
+        $this->notify(new verifyEmail($this));
+        
+    
+    }
+
+
 }
