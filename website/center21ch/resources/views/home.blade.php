42677526c527cc8c87c6b00cd12e716e3fa8f6dc
Ali <aalkinan@myune.edu.au>
2018-06-12 11:19:07 +1000

    create the notifies email

--- website/center21ch/resources/views/home.blade.php
+++ website/center21ch/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in!{{ auth()->user()->verified()? 'verified' : 'notVerified' }}
                 </div>
             </div>
         </div>
