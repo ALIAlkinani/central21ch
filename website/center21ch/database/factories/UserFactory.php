c70addf8b67ce4b7e6ae5612ecde129e79a4e430
Ali <aalkinan@myune.edu.au>
2018-06-21 04:38:23 +1000

    add loaded file to composer which allows us to call functions in function file

--- website/center21ch/database/factories/UserFactory.php
+++ website/center21ch/database/factories/UserFactory.php
@@ -20,4 +20,28 @@ $factory->define(App\User::class, function (Faker $faker) {
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
+  
+});
+
+$factory->define(App\Poem::class, function (Faker $faker) {
+    return [
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body'=>$faker->paragraph,
+    ];
+});
+
+$factory->define(App\Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        'poem_id' => function(){
+            return factory('App\Poem')->create()->id;
+        },
+       
+        'body'=>$faker->paragraph,
+    ];
 });
