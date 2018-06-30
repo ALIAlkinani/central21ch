d517aa163a75be2311deba8f886c76a9dfd1ac5a
Ali <aalkinan@myune.edu.au>
2018-06-30 12:23:32 +1000

    add channel list dropdown menu

--- website/center21ch/resources/views/layouts/app.blade.php
+++ website/center21ch/resources/views/layouts/app.blade.php
@@ -36,6 +36,18 @@
                     <ul class="navbar-nav mr-auto">
 
                             <li><a class="nav-link" href="/poems">Poems</a></li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  Channels
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                  @foreach (App\Channel::all() as $Channel )
+                                  <a class="dropdown-item" href="/poems/{{$Channel->slug}}">{{ $Channel->name }}</a>
+
+                                      
+                                  @endforeach
+                                </div>
+                              </li>
 
 
                     </ul>
