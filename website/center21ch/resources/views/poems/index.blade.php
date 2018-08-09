658a5b279405efd4f34805c5d4e877cfef1c9b5d
dprice8 <dprice8@myune.edu.au>
2018-08-09 23:03:52 +1000

    Remove 'before' from timestamps.

--- website/center21ch/resources/views/poems/index.blade.php
+++ website/center21ch/resources/views/poems/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
 
-   
     <div class="row">
         <div class="col-md-8">
          @forelse ($poems as $poem )
             <div class="card">               
 
                 <div class="card-header"> 
-                    <h6 class="flex"> <a href="{{ $poem->path() }}">{{ $poem->title }}</a> created by <a href="/poems?by={{ $poem->creator->name  }}">{{ $poem->creator->name }}</a> before {{ $poem->created_at->diffForhumans() }} </h6>
+                    <h6 class="flex"> <a href="{{ $poem->path() }}">{{ $poem->title }}</a> created by <a href="/poems?by={{ $poem->creator->name  }}">{{ $poem->creator->name }}</a> {{ $poem->created_at->diffForhumans() }} </h6>
                 <a href="{{ $poem->path() }}"> <span class="commit">  {{ $poem->replies_count }} {{ str_plural('Reply', $poem->replies_count) }}</span>
                 </a>
                 
@@ -21,7 +21,9 @@
 
                 </div>
             
-            </div> 
+            </div>
+                
+                
             <br>  
             @empty
                 <p> there are no relevent result at this time</p>
@@ -32,7 +34,6 @@
     </div>
 
     <br>
-
             
 </div>
 
