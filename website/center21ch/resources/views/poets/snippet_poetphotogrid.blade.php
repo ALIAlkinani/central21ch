9e40d8d207a4133f9d65ea3ecbb9b211c54761dc
dprice8 <dprice8@myune.edu.au>
2018-10-16 20:20:51 +1100

    Minor edits.

--- website/center21ch/resources/views/poets/snippet_poetphotogrid.blade.php
+++ website/center21ch/resources/views/poets/snippet_poetphotogrid.blade.php
@@ -1,7 +1,7 @@
 <aside class="photofromlastyear">
 
 		<h2>Poets</h2>
-		<p>A poet is a person who creates poetry. Poets may describe themselves as such or be described as such by others. A poet may simply be a writer of poetry, or may perform their art to an audience</p>
+		<p>A poet is a person who creates poetry. Poets may describe themselves as such or be described as such by others. A poet may simply be a writer of poetry, or may perform their art to an audience.</p>
 		<div class="modalphotos photogrid clearfix">
 				@foreach ($poems as $poem)
 				<a href="/poets/{{ $poem->arthur->id}}"><img class="img-thumbnail mr-1"  width="110"
